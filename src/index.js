bc6442f213cf91f1a5bf421aca09dca39c98e7ec
AlexKov90 <kavaleuski.alex@gmail.com>
2018-09-22 18:11:49 +0300

    implement sort class

--- src/index.js
+++ src/index.js
@@ -1,30 +1,48 @@
 class Sorter {
   constructor() {
     // your implementation
+      this.array = [];
+      this.comparator = (a, b) => (a-b);
   }
 
   add(element) {
     // your implementation
+      this.array.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.array[index];  
   }
 
   get length() {
     // your implementation
+      return this.array.length;
   }
 
   toArray() {
     // your implementation
+      return this.array;
   }
 
   sort(indices) {
     // your implementation
+      let subArray = [];
+      const sortedIndices = indices.sort(this.comparator);
+      
+      if (!Array.isArray(indices)) {
+          return this.array;
+      }
+      
+      sortedIndices.forEach(value => subArray.push(this.array[value]));
+      
+      subArray = subArray.sort(this.customComparator || this.comparator);
+    
+      sortedIndices.forEach((value, index) => this.array[value] = subArray[index] );
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.customComparator = compareFunction;
   }
 }
 
